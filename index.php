94587259ea992f1fe46e139f039b5e2b41b600da
bfearon <115654051+bfearon@users.noreply.github.com>
2022-10-18 00:21:59 -0700

    Added Link to Heroku

--- index.php
+++ index.php
@@ -30,4 +30,5 @@
     <button type = "button" class = "btn btn-primary"> CLICK ME </button>
     <button type = "button" class = "btn btn-success"> CLICK ME </button>
 
+    <a href="https://www.Heroku.com" target ="_blank" class = "btn btn-danger">Heroku.com</a>
     <?php require 'includes/footer.php' ?>
